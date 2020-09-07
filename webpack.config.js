bd2aa643507cb710874b865f7aaf1691ff3f7aef
Sebastian Rodriguez Marin (a.k.a @Seromarin) <rodriguez.marin.sebastian@gmail.com>
2020-09-06 22:45:27 -0500

    SCSS Webpack ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
+module.exports = {
     entry: {
         app: './src/ts/index.ts'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "js/[name].bundle.js",
+        filename: "js/[name].js",
     },
     devServer: {
         port: 3000,
@@ -15,6 +16,10 @@ const config = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.pug'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[id].css',
         }),
     ],
     resolve: {
@@ -33,14 +38,30 @@ const config = {
                 test: /\.tsx?$/,
                 loader: "ts-loader"
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    // "css-loader",
+                    // 'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            // Prefer `dart-sass`
+                            implementation: require('sass'),
+                        },
+                    },
+                ],
+            },
         ]
     }
 };
-module.exports = (env, argv) => {
-    if (argv.mode === 'development') { }
-    if (argv.mode === 'production') { }
-    return config;
-}
+// module.exports = (env, argv) => {
+//     if (argv.mode === 'development') { }
+//     if (argv.mode === 'production') { }
+//     return config;
+// }
 
 // module.exports = {
 //     // devtool: "inline-source-map",
