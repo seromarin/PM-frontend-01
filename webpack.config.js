ec5704d137293d10d2d45e7e1a5165d5c9a8f485
Sebastian Rodriguez Marin (a.k.a @Seromarin) <rodriguez.marin.sebastian@gmail.com>
2020-09-06 22:16:58 -0500

    Webpack Pug Ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,69 @@
-module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./src/ts/index.ts",
-    output: {
-      filename: "dist/js/bundle.js"
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: {
+        app: './src/ts/index.ts'
     },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "js/[name].bundle.js",
+    },
+    devServer: {
+        port: 3000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.pug'
+        }),
+    ],
     resolve: {
-      // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: [".ts", ".tsx", ".js"]
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
     },
     module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        {
-            test: /\.tsx?$/,
-            loader: "ts-loader"
-        },
-        {
-            test: /\.pug?$/,
-            loader: "pug-loader"
-        }
-      ]
+        rules: [
+            // all files with a `.pug` extension will be handled by `pug-loader`
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            },
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+        ]
     }
-  };
+};
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') { }
+    if (argv.mode === 'production') { }
+    return config;
+}
+
+// module.exports = {
+//     // devtool: "inline-source-map",
+//     context: path.resolve(__dirname, 'src'),
+//     // entry: "./src",
+//     // output: {
+//     //   filename: "dist"
+//     // },
+//     // resolve: {
+//     //   // Add `.ts` and `.tsx` as a resolvable extension.
+//     //   extensions: [".ts", ".tsx", ".js"]
+//     // },
+//     module: {
+//       rules: [
+//         // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+//         {
+//             test: /\.tsx?$/,
+//             loader: "ts-loader"
+//         },
+//         // {
+//         //     test: /\.pug?$/,
+//         //     loader: "pug-loader"
+//         // }
+//       ]
+//     }
+//   };
